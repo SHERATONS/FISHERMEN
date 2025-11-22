4077ab94edecd1e7460aeccd3cb82a1792953fdd
bbryram25 <nichakul.kon@student.mahidol.ac.th>
2025-11-22 20:12:03 +0700

    Update Cypress config path and clean up old files

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,14 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    
+    supportFile: "automated test cases/cypress/support/e2e.js",
+    
+   
+    specPattern: "automated test cases/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      
     },
   },
 });
